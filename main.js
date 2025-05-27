6c7fad9b479adf0e88ab11747929afe9a8d8afcd
Yin-han <yinhan.liu@student.uq.edu.au>
2025-05-28 02:04:38 +1000

    基本完成

--- main.js
+++ main.js
@@ -96,15 +96,3 @@
 // });
 
 
-<script>
-  const scrollItems = document.querySelectorAll('.scroll-fade');
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-      }
-    });
-  }, { threshold: 0.1 });
-
-  scrollItems.forEach(el => observer.observe(el));
-</script>
